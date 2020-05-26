e13cbd519974203152a2dcbbfa35971baabc1032
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-26 17:29:21 +1200

    Add app center dependencies & analytics(MainActivity)

--- app/src/main/java/com/paolomanlunas/jotter/MainActivity.kt
+++ app/src/main/java/com/paolomanlunas/jotter/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.paolomanlunas.jotter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
    override fun onCreate(savedInstanceState: Bundle?) {
       super.onCreate(savedInstanceState)
       setContentView(R.layout.activity_main)
+      // App Center:
+      AppCenter.start(application, "b69b0182-2837-42f2-b150-3e8c635f9002",
+              Analytics::class.java, Crashes::class.java)
    }
 }
