a6ce84454b3125edb91dd7daaf4320569e23b00b
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-26 23:16:17 +1200

    Add ViewBinding and Timber(logging lib) dependencies. Create 'JotterAppliation.kt' for Timber init.

--- app/src/main/java/com/paolomanlunas/jotter/MainActivity.kt
+++ app/src/main/java/com/paolomanlunas/jotter/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +15,7 @@ class MainActivity : AppCompatActivity() {
       // App Center:
       AppCenter.start(application, "b662e675-0625-41d9-8c89-90c16029d398",
               Analytics::class.java, Crashes::class.java)
+
+      Timber.d("onCreate CALLED!")
    }
 }
